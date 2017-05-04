b3458b6045dadf5c37ce68296a638e1b2044b404
alumneprova <alumneprova@PB1-12>
2017-05-04 19:47:08 +0200

    sdasadadas

---
+++ conversiones/Source.cpp
@@ -0,0 +1,44 @@
+/*
+int x,x2;		x[01...10...1]
+float y;	y[1....1(mantisa) 1....1...0(exp)]
+
+x=x+x2;	//el compilador verifica si puede hacer la operacion '+' para los tipos int y si se puede no da errores de compilacion
+x=x+y;	//hace la conversion implicita de int a float (operacion widening) para la 'x' y suma "x+y", pero al assignarlo a x que es un entero hace una conversion Narrowing donde va a perder datos al truncar el resultado de "x+y"
+y=x+y;
+
+conversiones explicitas(cast)
+
+static_cast
+	double d=1.21355;
+	int i = d;					//warning
+	int j = stitc_cast<int>(d);	//NO Warning
+
+	enum class Weapon{
+		FIST,
+		GUN,
+		SHOTGUN,
+		MAX
+	};
+	int m;
+	m = Weapon::FISTS; //error
+	m = static_cast<int> (Weapon::FISTS); //correcto
+
+dynamic_cast
+	?????????????
+const_cast
+	hace o quita el const de una variable
+	ej:
+	void Func(double& d){
+	//..
+	}
+	const double pi = 3.14;
+	Func(pi);						//error
+	Func(const_cast<double&>(pi));	//OK
+reinterpret_cast
+	int *p;
+	int i4;
+	i4 = p;							//error
+	i4 = static_cast<int>(p);		//error
+	i4 = reinterpret_cast<int>(p);	//OK
+	
+*/
---
+++ stack/Source.cpp
@@ -0,0 +1,149 @@
+/*
+stack(pila)->LIFO last in fisrt out
+top-	consulta del elemento de la cola (top())
+push-	inserta un elemento en el final de la pila (push_back())
+pop-	elimina el ultimo elemento de la pila (pop_back())
+size-	retorna el tamaño de la pila en un entero (size());
+
+declaracion:
+std::stack<int> d1;
+d1.push(1);
+d2.push(2);
+
+
+
+struct Person{
+	std::string name;
+	int age;
+
+	Person(std::string n, int a){
+		name = n;
+		age = a;
+	}
+}
+std:: stack<Person> s2;
+s2.push(person("ana", 20));
+s2.emplace("pepe", 22);
+
+std::stack<person>s3(s2); //constructor de copia
+
+*/
+
+#include <iostream>
+#include <algorithm>
+#include <stack>
+#include <deque>
+#include <queue>
+#include <string>
+
+
+//funcion que imprime una pila:
+void PrintStack(std::stack<std::string> a){
+	while (!a.empty()) {
+		std::cout << a.top() << std::endl;
+		a.pop();
+	}
+}
+
+
+//recibe 2 stacks ordeandos de mayor a menor y devuelve una sola pila ordenada:
+std::stack<int> SuperMeger(std::stack<int> a, std::stack<int> b) {
+	std::deque<int> aux;
+	std::stack<int> res;
+	while (!a.empty()) {
+		aux.push_back(a.top());
+		a.pop();
+	}
+	while (!b.empty()) {
+		aux.push_back(b.top());
+		b.pop();
+	}
+
+	std::sort(aux.begin(), aux.end());
+	for (int i = 0; i < aux.size(); i++) {
+		res.push(aux[i]);
+	}
+	//aux.clear();
+	return res;
+}
+
+std::stack<std::string> PutHat(std::stack<std::string> s1, std::stack<std::string> s2) {
+	std::stack<std::string> aux;
+	while (!s1.empty()) {
+		aux.push(s1.top());
+		s1.pop();
+	}
+	while (!aux.empty()) {
+		s2.push(aux.top());
+		aux.pop();
+	}
+	return s2;
+}
+
+std::queue<int> TransformToQ(std::stack<int> a) {
+	std::queue<int> res;
+	while (!a.empty()) {
+		res.push(a.top());
+		a.pop();
+	}
+	return res;
+}
+
+bool isHat(std::stack<int> a, std::stack<int> b) {
+	while (!a.empty()) {
+		if (a.top() != b.top())
+			return false;
+		a.pop();
+		b.pop();
+	}
+	return true;
+}
+
+bool balancedP(std::queue<char> a) {
+	int aux = 0;
+	while (!a.empty()) {
+		if (a.front() == '(')
+			aux++;
+		if (a.front() == ')')
+			aux--;
+		a.pop();
+		if (aux < 0)
+			return false;
+	}
+	return aux == 0;
+	
+}
+
+std::stack<int> change(std::stack<int> a) {
+	int top;
+	std::deque<int> aux;
+	top = a.top();
+	while (!a.empty()) {
+		aux.push_front(a.top());
+		a.pop();
+	}
+	top = aux.front();
+	aux[0] = aux[aux.size()];
+	aux[aux.size()] = top;
+	for (int i = 0; i < aux.size(); i++) {
+		a.push(aux[i]);
+	}
+}
+
+int main() {
+	std::stack<int> m1;
+	for (int i = 0; i < 10; i++) {
+		m1.push(i);
+	}
+	std::stack<int> m2(m1);
+
+	//PrintStack(SuperMeger(m1, m2));
+
+	std::stack<std::string> p1({"7","4","9","2"});
+	std::stack<std::string> p2({ "5","5","6"});
+
+	std::stack<std::string>p3 = PutHat(p1, p2);
+	PrintStack(p3);
+
+	return 0;
+}
